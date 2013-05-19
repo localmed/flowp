a9deede3993d1a6da60659842d278916c0a3d799
Paweł Gałązka <pawel.galazka@pracli.com>
2013-05-19 19:28:24 +0200

    New module flowp.system

---
+++ flowp/system.py
@@ -0,0 +1,127 @@
+from flowp import ftypes
+import subprocess
+import os
+import shutil
+import subprocess
+
+
+class Process(ftypes.Object):
+    """Manage structure of process / command
+    Example:
+        
+        p = Process('./some/script')
+        p.arg('--test1', 1)
+        p.arg('--test2', ['a', 'b'])
+        p.arg('test3')
+        p.command # -> ./some/script --test1 1 --test2 a b test3
+        p.run()
+    """
+    def __init__(self, process_name):
+        """Create base of command structure as list"""
+        self.elements = ftypes.List([process_name])
+
+    def arg(self, kv, v=None):
+        """Add arguments to the process
+        :param kv:
+            argument key/name or value right away
+        :param v:
+            argument value. It can be list/tuple or
+            single value object.
+        """
+        def func(item):
+            if item.isinstance(tuple) and item.len and item[0] == kv:
+                return (kv, v)
+            else:
+                return item
+
+        if v:
+            if self.elements.filter(
+                    lambda el: el.isinstance(tuple) and el[0] == kv):
+                self.elements.map_it(func)
+            else:
+                self.elements.append((kv, v))
+        else:
+            self.elements.append(kv) 
+
+    @property
+    def command(self):
+        """Return constructed command as string
+        :rtype str:
+        """
+        return self.elements.flatten.flatten.join(' ')
+
+    def run(self):
+        """Run constructed command as process"""
+        def func(item):
+            if item.isinstance(str):
+                return item
+            else:
+                return item.str
+
+        subprocess.check_call(self.elements.flatten.map(func))
+
+
+class cd(object):
+    """Change working directory. Behave exacly like os.chdir, except
+    that it can be used as context manager.
+    Example:
+
+        with cd('some/path'):
+            # do some operations
+            pass
+
+    After exiting the context, return to the oryginal working directory.
+    This also works but doesn't return to the original working dir:
+
+        cd('some/path')
+    """
+    def __init__(self, path):
+        self.org_dir = os.getcwd()
+        os.chdir(path) 
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        os.chdir(self.org_dir)
+
+
+# Alias for shutil.move
+mv = shutil.move
+
+# Alias for os.mkdir
+mkdir = os.makedirs
+
+# Alias for os.unlink
+rm = os.unlink
+
+
+def sh(command):
+    """Executes shell command as is
+    :param str command:
+        command to execute
+    """ 
+    subprocess.check_call(command, shell=True)
+
+import pdb
+class FlowpFileExecuter(ftypes.Object):
+    def __init__(self, args):
+        self.args = args
+
+    @property
+    def tasks(self):
+        def func(item):
+            if item.len == 1:
+                return [item[0], []]
+            else:
+                return [item[0], item[1].split(',')]
+
+        tasks = ftypes.List(self.args[1:])
+        tasks.map_it(lambda x: x.split(':'))
+        tasks.map_it(func)
+        return tasks.dict
+
+    def execute(self):
+        flowpfile = __import__('flowpfile', globals(), locals(), [], -1) 
+        for task, args in self.tasks.items():
+            getattr(flowpfile, task)(*args) 
---
+++ spec/spec_system.py
@@ -0,0 +1,132 @@
+from flowp.testing import Behavior
+from flowp import system
+import mock
+import os
+import shutil
+
+
+class Process(Behavior):
+    def before_each(self):
+        self.p = system.Process('testproc')
+
+    def it_takes_basic_process_name_as_argument(self):
+        pass
+
+    def it_add_process_arguments(self):
+        self.p.arg('--test1', 1)
+        self.p.arg('--test2', ['a', 'b'])
+
+    def it_generate_process_command_with_given_arguments_in_order(self):
+        self.p.arg('--test1', 1)
+        self.p.arg('--test2', ['a', 'b'])
+        self.p.arg('test3')
+        self.p.arg('t4', 't5')
+        assert self.p.command == 'testproc --test1 1 --test2 a b test3 t4 t5'
+
+    def it_update_arguments_values_without_changing_args_order(self):
+        self.p.arg('-a', 1)
+        self.p.arg('-b', 2)
+        assert self.p.command == 'testproc -a 1 -b 2'
+        self.p.arg('-a', 3)
+        assert self.p.command == 'testproc -a 3 -b 2'
+
+    @mock.patch('subprocess.check_call')
+    def it_run_process(self, subpr_mock):
+        self.p.arg('-a', 1)
+        self.p.arg('b')
+        assert not subpr_mock.called
+        self.p.run()
+        assert subpr_mock.called
+        subpr_mock.assert_called_once_with(['testproc', '-a', '1', 'b'])
+
+
+class FilesFunction(Behavior):
+    def it_returns_files_list_by_path_star_pattern(self):
+        self.fail()
+
+
+class FileUtils(Behavior):
+    def before_each(self):
+        self.org_working_dir = os.getcwd() 
+        if os.path.isdir('spec/tmp'):
+            shutil.rmtree('spec/tmp')
+        os.mkdir('spec/tmp')
+        os.chdir('spec/tmp')
+
+    def after_each(self):
+        os.chdir(self.org_working_dir) 
+        shutil.rmtree('spec/tmp')
+
+    def it_have_cd_function_which_enter_to_directories(self):
+        os.mkdir('testdir')
+        assert os.getcwd().endswith('tmp')
+        system.cd('testdir')
+        assert os.getcwd().endswith('tmp/testdir')
+
+        # also act like a context manager
+        os.chdir(self.org_working_dir + '/spec/tmp')
+        assert os.getcwd().endswith('tmp')
+        with system.cd('testdir'):
+            assert os.getcwd().endswith('tmp/testdir')
+        assert os.getcwd().endswith('tmp') 
+
+    def it_have_mv_function_which_move_files(self):
+        os.mkdir('testdir') 
+        with open('testfile', 'w'):
+            assert os.path.isfile('testfile') 
+            assert not os.path.isfile('testdir/testfile') 
+            system.mv('testfile', 'testdir/testfile')
+            assert not os.path.isfile('testfile') 
+            assert os.path.isfile('testdir/testfile') 
+
+    def it_have_mkdir_function_which_creates_directory(self):
+        assert not os.path.isdir('testdir')
+        system.mkdir('testdir')
+        assert os.path.isdir('testdir')
+        assert not os.path.isdir('testdir/testdir2/testdir3')
+        system.mkdir('testdir/testdir2/testdir3')
+        assert os.path.isdir('testdir/testdir2/testdir3')
+
+    def it_have_rm_function_which_remove_files(self):
+        with open('testfile', 'w'):
+            assert os.path.isfile('testfile')
+            system.rm('testfile')
+            assert not os.path.isfile('testfile')
+
+    @mock.patch('subprocess.check_call')
+    def it_have_sh_function_which_executes_shell_command(self, subpr_mock):
+        system.sh('test shell command')
+        subpr_mock.assert_called_once_with('test shell command', shell=True)
+
+
+class FlowpFileExecuter(Behavior):
+    def before_each(self):
+        self.args1 = ['script', 'task1', 'task2']
+        self.args2 = ['script', 'task1:abc', 'task2']
+        self.args3 = ['script', 'task1', 'task2:ab,cd']
+        self.E = system.FlowpFileExecuter
+
+    def it_parse_script_arguments_as_tasks_list_with_arguments(self):
+        assert self.E(self.args1).tasks == {'task1': [], 'task2': []}
+        assert self.E(self.args2).tasks == {'task1': ['abc'], 'task2': []}
+        assert self.E(self.args3).tasks == {'task1': [], 'task2': ['ab', 'cd']}
+
+    @mock.patch('__builtin__.__import__')
+    def it_executes_tasks_from_flowpfile_by_given_script_arguments(self, im_mock):
+        flowpfile_mock = mock.Mock()
+        im_mock.return_value = flowpfile_mock
+
+        self.E(self.args1).execute()
+        assert im_mock.call_args[0][0] == 'flowpfile'
+        assert flowpfile_mock.task1.assert_called_with()
+        assert flowpfile_mock.task2.assert_called_with()
+
+        self.E(self.args2).execute()
+        flowpfile_mock.reset_mock()
+        assert flowpfile_mock.task1.assert_called_with('abc')
+        assert flowpfile_mock.task2.assert_called_with()
+
+        self.E(self.args3).execute()
+        flowpfile_mock.reset_mock()
+        assert flowpfile_mock.task1.assert_called_with()
+        assert flowpfile_mock.task2.assert_called_with('ab', 'cd')
