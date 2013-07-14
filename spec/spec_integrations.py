4e3fa5aab0609d32e6ce53f5dce289f9ff794987
Paweł Gałązka <pawel.galazka@pracli.com>
2013-07-14 12:40:35 +0200

    Additional integrations tests + introducing expect expressions to them

--- spec/spec_integrations.py
+++ spec/spec_integrations.py
@@ -1,5 +1,5 @@
 from flowp import ftypes
-from flowp.testing import Behavior
+from flowp.testing import Behavior, expect
 
 
 class Ftypes(Behavior):
@@ -8,16 +8,21 @@ class Ftypes(Behavior):
         self.l = ['a', 'b', 'c', 'd', 'e']
         self.nl = [1, 2, [3, 4], 5, [1, 2], 3]
 
+        self.fs = ftypes.Str(self.s)
+        self.fl = ftypes.List(self.l)
+        self.fnl = ftypes.List(self.nl)
+        self.fl2 = ftypes.List(['1', '2', '1', '1', '3', '2', '4'])
+
     def it_invokes_types_method_through_this_wrapper(self):
         assert ftypes.this(self.s).isinstance(ftypes.Str)
         assert ftypes.this(self.l).hasattr('index')
 
     def it_do_methods_chain_operations(self):
-        s = ftypes.Str(self.s)
-        l = ftypes.List(self.l)
-        nl = ftypes.List(self.nl)
-        l2 = ftypes.List(['1', '2', '1', '1', '3', '2', '4'])
+        expect(self.fs.split('-').reversed.join('.')) == 'ghi.def.abc'
+        expect(self.fl.filter(lambda x: x != 'c')) == ['a', 'b', 'd', 'e']
+        expect(self.fl2.set.map(lambda x: x.int)) == ftypes.Set([1, 2, 3, 4])
 
-        assert s.split('-').reversed.join('.') == 'ghi.def.abc'
-        assert l.filter(lambda x: x != 'c') == ['a', 'b', 'd', 'e']
-        assert l2.set.map(lambda x: x.int) == ftypes.Set([1, 2, 3, 4])
+    def it_do_methods_chain_operation_on_builtin_types_methods(self):
+        expect(self.fnl.flatten.uniq.count(1).str) == '1'
+        expect(self.fs.replace('b', 'z').isinstance(ftypes.Str)).ok
+        expect(self.fs.hasattr('rfind').isinstance(ftypes.BoolAdapter)).ok
