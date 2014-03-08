391aab26c552df02788c384aa833ae6e87915539
Paweł Gałązka <pawel.galazka@pracli.com>
2014-03-08 01:29:32 +0100

    flowp.files: copy util

--- flowp/files.py
+++ flowp/files.py
@@ -1,5 +1,8 @@
 import os
 import subprocess
+import shutil
+import glob
+import os.path
 
 
 class cd:
@@ -41,3 +44,26 @@ def mkdir(dirname):
 def sh(command):
     """Executes shell command as is"""
     subprocess.check_call(command, shell=True)
+
+
+exists = os.path.exists
+
+
+def cp(src, dst):
+    """Copy files and directories::
+
+        cp('dir/file.py', 'dir2/file.py')
+        cp('dir/*.py', 'dir2')
+        cp(['file1.py', 'dir/*.py'], 'dir')
+    """
+    if '*' in src:
+        src = glob.glob(src)
+
+    if isinstance(src, list):
+        for f in src:
+            cp(f, dst)
+    else:
+        if os.path.isdir(src):
+            shutil.copytree(src, dst)
+        else:
+            shutil.copy(src, dst)
--- spec/spec_files.py
+++ spec/spec_files.py
@@ -1,5 +1,5 @@
 from flowp.testing import Behavior, expect, skip
-from flowp import files
+from flowp.files import cd, touch, mkdir, cp, sh, exists
 import os
 
 
@@ -12,11 +12,11 @@ class Cd(Behavior):
         self.tmpdir.exit()
 
     def it_changes_working_directory(self):
-        files.cd('testdir')
+        cd('testdir')
         expect(os.getcwd().endswith('testdir')).to_be(True)
 
     def it_changes_working_directory_in_context(self):
-        with files.cd('testdir'):
+        with cd('testdir'):
             expect(os.getcwd().endswith('testdir')).to_be(True)
         expect(os.getcwd().endswith('testdir')).to_be(False)
 
@@ -30,7 +30,7 @@ class Touch(Behavior):
 
     def it_creates_empty_file(self):
         expect(os.path.isfile('testfile')).to_be(False)
-        files.touch('testfile')
+        touch('testfile')
         expect(os.path.isfile('testfile')).to_be(True)
 
 
@@ -43,35 +43,57 @@ class Mkdir(Behavior):
 
     def it_creates_empty_directory(self):
         expect(os.path.isdir('testdir')).to_be(False)
-        files.mkdir('testdir')
+        mkdir('testdir')
         expect(os.path.isdir('testdir')).to_be(True)
 
 
-@skip
 class Cp(Behavior):
     def before_each(self):
         self.tmpdir.enter()
+        touch('file0.py')
+        mkdir('testdir1')
+        touch('testdir1/file1.py')
+        touch('testdir1/file2.py')
+        mkdir('testdir2')
 
-    def before_each(self):
+    def after_each(self):
         self.tmpdir.exit()
 
-    class WhenFilenameInSourceGiven(Behavior):
-        def it_test_something(self):
-            pass
-        pass
+    class WhenFilenameGiven(Behavior):
+        def it_copy_single_file(self):
+            cp('testdir1/file1.py', 'testdir2/file1b.py')
+            expect(exists('testdir1/file1.py')).to_be(True)
+            expect(exists('testdir2/file1b.py')).to_be(True)
 
-    class WhenGlobPatternInSourceGiven(Behavior):
-        pass
+    class WhenGlobPatternGiven(Behavior):
+        def it_copy_group_of_files(self):
+            cp('testdir1/*.py', 'testdir2')
+            expect(exists('testdir2/file1.py')).to_be(True)
+            expect(exists('testdir2/file2.py')).to_be(True)
 
-    class WhenArrayOfFilenamesInSourceGiven(Behavior):
-        pass
+    class WhenArrayOfFilenamesGiven(Behavior):
+        def it_copy_group_of_files(self):
+            cp(['testdir1/file1.py', 'testdir1/file2.py'], 'testdir2')
+            expect(exists('testdir2/file1.py')).to_be(True)
+            expect(exists('testdir2/file2.py')).to_be(True)
 
-    class WhenArrayOfFilenamesAndGlobPatternsInSourceGiven(Behavior):
-        pass
+    class WhenArrayOfFilenamesAndGlobPatternsGiven(Behavior):
+        def it_copy_group_of_files(self):
+            cp(['file0.py', 'testdir1/*.py'], 'testdir2')
+            expect(exists('testdir2/file0.py')).to_be(True)
+            expect(exists('testdir2/file1.py')).to_be(True)
+            expect(exists('testdir2/file2.py')).to_be(True)
+
+    class WhenDirectoryGiven(Behavior):
+        def it_copy_whole_directory(self):
+            cp('testdir1', 'testdir3')
+            expect(exists('testdir3/file1.py')).to_be(True)
+            expect(exists('testdir3/file2.py')).to_be(True)
+
 
 
 class Sh(Behavior):
     def it_executes_shell_commands(self):
         ccall = self.mock('subprocess.check_call')
-        files.sh('test shell command')
+        sh('test shell command')
         expect(ccall).to_have_been_called_with('test shell command', shell=True)
